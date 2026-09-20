5637efe066d171327c2462628abf71a3069bc6ce
AdnanMahmoudiDev <adnan.mahmoudi.dev@gmail.com>
2026-09-20 12:43:04 +0330

    Fix trusted proxy HTTPS detection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,10 +13,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'admin' => AdminMiddleware::class,
-            ]);
-        })
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
